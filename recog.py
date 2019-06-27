cebb2959402b047a5ef2b2e2ba3dd8acf28f73b8
AhmedMousa2020 <34624785+AhmedMousa2020@users.noreply.github.com>
2019-06-27 02:02:28 +0200

    Add files via upload

---
+++ recog.py
@@ -0,0 +1,25 @@
+'''from PIL import Image, ImageOps
+
+original_image = Image.open("image.jpg")
+size = (48, 48)
+fit_and_resized_image = ImageOps.fit(original_image, size, Image.ANTIALIAS)
+fit_and_resized_image.save("new.jpg")'''
+
+
+import cv2
+import numpy as np
+
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap=cv2.VideoCapture(0)
+
+while(True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
